9a79e3753edf8189ca13cbc895daf2e3a7443ef3
John Ouellet <labboy0276@yahoo.com>
2017-11-11 22:21:14 -0500

    Strip scss, fix page.twig

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,12 +1,3 @@
 (function ($) {
 
-  Drupal.behaviors.adjustTable = {
-    attach: function() {
-      // Adjust oddness that is getting added to the table.
-      if($('table.field-lake').length) {
-        $('table.field-lake th').removeClass('tabledrag-hide').removeAttr('style');
-      }
-    }
-  }
-
 })(jQuery);
